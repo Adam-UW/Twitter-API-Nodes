03190ada33dfe716a14e2f2e2b20cdaec7a30a35
Adam Shandi <abbashasan91@gmail.com>
2020-06-03 23:53:39 -0700

    reply for followers working

--- bot.js
+++ bot.js
@@ -1,32 +1,57 @@
 const Twit = require('twit')
 const config = require('./config')
 
-
 var T = new Twit(config)
+
+
+let stream = T.stream('user')
+
+stream.on('follow', followed)
+
+ 
+
+function followed(event){
+    let name= event.source.name
+    let screenName= event.source.screen_name
+    tweetIt('@' + screenName + 'Thx for following me')
+}
+
+
+
+
+
+
+
+
+// tweet each 20 seconds
+// setInterval(tweetIt, 1000*20)
+
+// A function to POST a tweet
+function tweetIt(txt){
+
+    var random= Math.floor(Math.random()*100 +1) 
+    let tweet={
+       // status:`A random number  ${random}#codingjavascript from nodejs`
+       status: txt
+    }
+    T.post('statuses/update', tweet, tweeted)
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('Error occured')
+        }
+        console.log('POST is working..')
+    }
+}
+
 
 let parms={
     q:'javascript',
     count: 5
 }
 
-let tweet={
-    status:"#codingjavascript from nodejs"
-}
 
 T.get('search/tweets', parms, gotData)
-T.post('statuses/update', tweet, tweeted)
-
-function tweeted(err, data, response){
-    if(err){
-        console.log('Error occured')
-    }
-    console.log('POST is working..')
-}
-
-
-
-
-
 
 function gotData(err, data, response){
     let tweets= data.statuses
